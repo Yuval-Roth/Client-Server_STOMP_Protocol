f634a7d9243d8be6852c6449ac33714f4f17065d
Yuval <rothyuv@post.bgu.ac.il>
2022-12-30 17:19:44 +0200

    commented out Server.java

--- kvatsim_mukhashim/Server.java
+++ kvatsim_mukhashim/Server.java
@@ -1,60 +1,57 @@
+// import bgu.spl.net.api.MessageEncoderDecoder;
+// import bgu.spl.net.api.MessagingProtocol;
+// import java.io.Closeable;
+// import java.util.function.Supplier;
 
+// public interface Server<T> extends Closeable {
 
-import bgu.spl.net.api.MessageEncoderDecoder;
-import bgu.spl.net.api.MessagingProtocol;
-import java.io.Closeable;
-import java.util.function.Supplier;
+//     /**
+//      * The main loop of the server, Starts listening and handling new clients.
+//      */
+//     void serve();
 
-public interface Server<T> extends Closeable {
+//     /**
+//      *This function returns a new instance of a thread per client pattern server
+//      * @param port The port for the server socket
+//      * @param protocolFactory A factory that creats new MessagingProtocols
+//      * @param encoderDecoderFactory A factory that creats new MessageEncoderDecoder
+//      * @param <T> The Message Object for the protocol
+//      * @return A new Thread per client server
+//      */
+//     public static <T> Server<T>  threadPerClient(
+//             int port,
+//             Supplier<MessagingProtocol<T> > protocolFactory,
+//             Supplier<MessageEncoderDecoder<T> > encoderDecoderFactory) {
 
-    /**
-     * The main loop of the server, Starts listening and handling new clients.
-     */
-    void serve();
+//         return new BaseServer<T>(port, protocolFactory, encoderDecoderFactory) {
+//             @Override
+//             protected void execute(BlockingConnectionHandler<T>  handler) {
+//                 new Thread(handler).start();
+//             }
 
-    /**
-     *This function returns a new instance of a thread per client pattern server
-     * @param port The port for the server socket
-     * @param protocolFactory A factory that creats new MessagingProtocols
-     * @param encoderDecoderFactory A factory that creats new MessageEncoderDecoder
-     * @param <T> The Message Object for the protocol
-     * @return A new Thread per client server
-     */
-    public static <T> Server<T>  threadPerClient(
-            int port,
-            Supplier<MessagingProtocol<T> > protocolFactory,
-            Supplier<MessageEncoderDecoder<T> > encoderDecoderFactory) {
-
-        return new BaseServer<T>(port, protocolFactory, encoderDecoderFactory) {
-            @Override
-            protected void execute(BlockingConnectionHandler<T>  handler) {
-                new Thread(handler).start();
-            }
-
-            @Override
-            public void serve() {
-                // TODO Auto-generated method stub
+//             @Override
+//             public void serve() {
                 
-            }
-        };
+//             }
+//         };
 
-    }
+//     }
 
-    /**
-     * This function returns a new instance of a reactor pattern server
-     * @param nthreads Number of threads available for protocol processing
-     * @param port The port for the server socket
-     * @param protocolFactory A factory that creats new MessagingProtocols
-     * @param encoderDecoderFactory A factory that creats new MessageEncoderDecoder
-     * @param <T> The Message Object for the protocol
-     * @return A new reactor server
-     */
-    public static <T> Server<T> reactor(
-            int nthreads,
-            int port,
-            Supplier<MessagingProtocol<T>> protocolFactory,
-            Supplier<MessageEncoderDecoder<T>> encoderDecoderFactory) {
-        return new Reactor<T>(nthreads, port, protocolFactory, encoderDecoderFactory);
-    }
+//     /**
+//      * This function returns a new instance of a reactor pattern server
+//      * @param nthreads Number of threads available for protocol processing
+//      * @param port The port for the server socket
+//      * @param protocolFactory A factory that creats new MessagingProtocols
+//      * @param encoderDecoderFactory A factory that creats new MessageEncoderDecoder
+//      * @param <T> The Message Object for the protocol
+//      * @return A new reactor server
+//      */
+//     public static <T> Server<T> reactor(
+//             int nthreads,
+//             int port,
+//             Supplier<MessagingProtocol<T>> protocolFactory,
+//             Supplier<MessageEncoderDecoder<T>> encoderDecoderFactory) {
+//         return new Reactor<T>(nthreads, port, protocolFactory, encoderDecoderFactory);
+//     }
 
-}
+// }
