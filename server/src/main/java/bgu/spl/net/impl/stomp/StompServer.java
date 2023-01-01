bb4f87da42469d27e19b79d1fe8481165baa6681
Yuval <rothyuv@post.bgu.ac.il>
2023-01-01 02:12:43 +0200

    Added shit that copilot recommended

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -7,6 +7,30 @@ import bgu.spl.net.srv.ThreadPerClient;
 
 public class StompServer{
     public static void main(String[] args) {
+
+        if(args.length != 2){
+            System.out.println("Usage: java StompServer <port> <Reactor | ThreadPerClient>");
+            return;
+        }
+        if(args[0].matches("[0-9]+"))
+            System.out.println("Port: " + args[0]);
+        else{
+            System.out.println("Invalid port");
+            System.out.println("Usage: java StompServer <port> <Reactor | ThreadPerClient>");
+            return;
+        }
+        
+        if(args[1].toLowerCase().equals("reactor"))
+            System.out.println("Starting Reactor Server...");
+        else if(args[1].toLowerCase().equals("threadperclient"))
+            System.out.println("Starting Thread Per Client Server...");
+        else{
+            System.out.println("Invalid server type");
+            System.out.println("Usage: java StompServer <port> <Reactor | ThreadPerClient>");
+            return;
+        }
+
+
         int port = Integer.parseInt(args[0]);
         BaseServer<String> server;
 
