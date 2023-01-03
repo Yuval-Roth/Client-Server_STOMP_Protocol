843a8cab533948ecf00b942ed2bceb1919efd2c1
Yuval <rothyuv@post.bgu.ac.il>
2023-01-03 02:42:22 +0200

    significant revamp of ErrorFrame static methods

--- server/src/main/java/bgu/spl/net/impl/stomp/Backend/Frames/ErrorFrame.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/Backend/Frames/ErrorFrame.java
@@ -8,7 +8,13 @@ public class ErrorFrame extends Frame {
         super(headers, frameBody, StompCommand.ERROR);
     }
 
-    public static Frame generateErrorFrame(int receiptId,String message,String receivedFrame) {
+    /**
+     * @param receiptId - if receipt-id => 0, receipt-id header will be added
+     * @param message - the message to be sent in the header
+     * @param body - the body of the error frame
+     * @param bodyIsBadFrame - if true, the body will be added as the frame that caused the error
+     */
+    public static Frame generateErrorFrame(int receiptId,String message,String body, boolean bodyIsBadFrame) {
 
         String frame = "ERROR"+NEW_LINE; // command
         if(receiptId >= 0)
@@ -17,36 +23,80 @@ public class ErrorFrame extends Frame {
         frame += NEW_LINE;//end of headers
         
         //body
-        if(receivedFrame.length() > 0){
-            frame += "The frame received was:"+NEW_LINE;
-            frame += "========================"+NEW_LINE;
-            frame += receivedFrame.replace("^@", "")+NEW_LINE;
-            frame += "========================"+NEW_LINE;
+        if(body.length() > 0){
+            if(bodyIsBadFrame){
+                frame += "The frame received was:"+NEW_LINE;
+                frame += "========================"+NEW_LINE;
+                frame += body.replace("^@", "")+NEW_LINE;
+                frame += "========================"+NEW_LINE;    
+            }
+            else{
+                frame += body.replace("^@", "")+NEW_LINE;
+            }
         }
         frame += END_OF_FRAME;
 
         return Frame.parse(frame);
     }
     
-    public static Frame generateErrorFrame(String message,String body) {
-
-        return generateErrorFrame(-1, message, body);
-    }
-
-    public static Frame generateErrorFrame(String message,Frame badFrame) {
-
-        return generateErrorFrame(message, badFrame.getFrameString());
-    }
-
-    public static Frame generateErrorFrame(int receiptId,String message,Frame badFrame) {
-
-        return generateErrorFrame(message, badFrame.getFrameString());
-    }
-
+    /**
+     * This overload does not add a body and does not add a receipt-id header
+     * @param message - the message to be sent in the header
+     * @return
+     */
     public static Frame generateErrorFrame(String message) {
 
-        return generateErrorFrame(-1, message, "");
+        return generateErrorFrame(-1, message, "", false);
     }
+
+    /**
+     * This overload does not add the body as a normal string. It does not add a receipt-id header
+     * 
+     * @param message - the message to be sent in the header
+     * @param body - the body of the error frame
+     */
+    public static Frame generateErrorFrame(String message,String body) {
+
+        return generateErrorFrame(-1, message, body,false);
+    }
+
+    /**
+     * this overload does not add a receipt-id header
+     * 
+     * @param message - the message to be sent in the header
+     * @param body - the body of the error frame
+     * @param bodyIsBadFrame - if true, the body will be added as the frame that caused the error, 
+     * if false, the body will be added as a regular string
+     */
+    public static Frame generateErrorFrame(String message,String body,boolean bodyIsBadFrame) {
+
+        return generateErrorFrame(-1, message, body, bodyIsBadFrame);
+    }
+
+    /**
+     * This overload adds the frame that caused the error as the body of the error frame. 
+     * It does not add a receipt-id header
+     * 
+     * @param message - the message to be sent in the header
+     * @param badFrame - the frame that caused the error
+     */
+    public static Frame generateErrorFrame(String message,Frame badFrame) {
+
+        return generateErrorFrame(-1,message, badFrame.getFrameString(), true);
+    }
+
+    /**
+     * This overload adds the frame that caused the error to the body of the error frame.
+     * 
+     * @param receiptId - if receipt-id => 0, receipt-id header will be added
+     * @param message - the message to be sent in the header
+     * @param badFrame - the frame that caused the error
+     */
+    public static Frame generateErrorFrame(int receiptId,String message,Frame badFrame) {
+
+        return generateErrorFrame(receiptId,message, badFrame.getFrameString(),true);
+    }
+
 
     
 }
