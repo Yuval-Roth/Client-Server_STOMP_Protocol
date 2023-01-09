86f666e6a5515343195c6e9b2b53faafc8bc9525
Peleg <benbapel@post.bgu.ac.il>
2023-01-09 17:34:41 +0200

    :bug: Changed a couple of variables to be non-const

--- client/include/UserData.h
+++ client/include/UserData.h
@@ -6,10 +6,18 @@
 class UserData{
 
     private:
+    static UserData* instance;
+    UserData();
+    ~UserData();
+    bool loggedIn;
+    string userName;
 
     public:
 
     void addAction(Frame frame);
     void setUserName();
+    void setLoggedIn(bool loggedIn);
+    //rule of 3
+    
 
 };
--- client/src/Frames/ConnectedFrame.cpp
+++ client/src/Frames/ConnectedFrame.cpp
@@ -1,6 +1,15 @@
 #include "ConnectedFrame.h"
+#include "UserData.h"
 
 ConnectedFrame::ConnectedFrame(StompCommand command, unordered_map<string, string> headers, string frameBody)
     : Frame(command, headers, frameBody)
 {
+    
+}
+
+
+void ConnectedFrame::execute(ConnectionHandler &connectionHandler) {
+    userData.setLoggedIn(true);
+    cout << "Login successful!" << endl;
+
 }
