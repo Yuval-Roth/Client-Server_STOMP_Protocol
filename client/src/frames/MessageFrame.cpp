4f3ae8df1d95e876da128fdf6d5d680d8a249025
Yuval Roth <rothyuv@post.bgu.ac.il>
2023-01-11 01:51:19 +0200

    updates

--- client/src/frames/MessageFrame.cpp
+++ client/src/frames/MessageFrame.cpp
@@ -1,9 +1,12 @@
 #include "MessageFrame.h"
+#include "UserData.h"
+#include "../include/event.h"
 
 MessageFrame::MessageFrame(StompCommand command, unordered_map<string, string> headers, string frameBody)
     : ExecutableFrame(command, headers, frameBody){}
 
 void MessageFrame::execute()
 {
-    //TODO
+    cout<<frameBody<<endl;
+    userData.addGameEvent(new Event(frameBody));
 }
--- client/src/frames/SendFrame.cpp
+++ client/src/frames/SendFrame.cpp
@@ -12,18 +12,20 @@ SendFrame *SendFrame::get(Event& event)
     UserData &userData = UserData::getInstance();
     unordered_map<string, string> headers;
     headers["destination"] = "/" + event.get_team_a_name() + "_" + event.get_team_b_name();
-    headers["user"] = userData.getUserName();
-    headers["event name"] = event.get_name();
-    headers["time"] = to_string(event.get_time());
+//    headers["user"] = userData.getUserName();
+//    headers["event name"] = event.get_name();
+//    headers["time"] = to_string(event.get_time());
+//
+//    map<string, string> gameUpdates = event.get_game_updates();
+//    string gameUpdatesString;
+//    for (auto & gameUpdate : gameUpdates) {
+//        gameUpdatesString += gameUpdate.first + ":" + gameUpdate.second + ",";
+//    }
+//    // copilot generated - seems like an iterator over the map and seems to do the trick. check if this the right output
+//    headers["game updates"] = gameUpdatesString;
+//    headers["description"] = event.get_description();
 
-    map<string, string> gameUpdates = event.get_game_updates();
-    string gameUpdatesString;
-    for (auto & gameUpdate : gameUpdates) {
-        gameUpdatesString += gameUpdate.first + ":" + gameUpdate.second + ",";
-    }
-    // copilot generated - seems like an iterator over the map and seems to do the trick. check if this the right output
-    headers["game updates"] = gameUpdatesString;
-    headers["description"] = event.get_description();
-    SendFrame *frame = new SendFrame(StompCommand::SEND, headers, "");
+    string body = event.toJson();
+    SendFrame *frame = new SendFrame(StompCommand::SEND, headers, body);
     return frame;
 }
