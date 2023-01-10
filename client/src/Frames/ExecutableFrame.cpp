7fc33ba95c1e80c9ffbedf4f4e1c76eb7c2b8ad3
Yuval <rothyuv@post.bgu.ac.il>
2023-01-10 14:10:54 +0200

    initial implementation of unsubscribe::get

--- client/src/Frames/ExecutableFrame.cpp
+++ client/src/Frames/ExecutableFrame.cpp
@@ -1,9 +1,7 @@
 #include "ExecutableFrame.h"
 
 ExecutableFrame::ExecutableFrame(StompCommand command, unordered_map<string, string> headers, string frameBody)
-    : Frame(command, headers, frameBody)
-{
-}
+    : Frame(command, headers, frameBody){}
 
 ExecutableFrame *ExecutableFrame::parse(string messageToParse)
 {
