9a3877557a64942df931e759bf83195bd99a1401
Peleg <benbapel@post.bgu.ac.il>
2023-01-10 18:26:25 +0200

    Implemented SEND frame factory

--- client/src/frames/SendFrame.cpp
+++ client/src/frames/SendFrame.cpp
@@ -1,6 +1,7 @@
 #include "SendFrame.h"
 #include "event.h"
 #include <unordered_map>
+#include "UserData.h"
 
 SendFrame::SendFrame(StompCommand command, unordered_map<string, string> headers, string frameBody)
     : Frame(command, headers, frameBody){}
@@ -8,9 +9,21 @@ SendFrame::SendFrame(StompCommand command, unordered_map<string, string> headers
 
 SendFrame *SendFrame::get(Event event)
 {
-    // TODO: implement
+    UserData &userData = UserData::getInstance();
+    unordered_map<string, string> headers;
+    headers["destination"] = "/" + event.get_team_a_name() + "_" + event.get_team_b_name();
+    headers["user"] = userData.getUserName();
+    headers["event name"] = event.get_name();
+    headers["time"] = to_string(event.get_time());
 
-    event.get_name();
-
-    return nullptr;
+    map<string, string> gameUpdates = event.get_game_updates();
+    string gameUpdatesString;
+    for (auto & gameUpdate : gameUpdates) {
+        gameUpdatesString += gameUpdate.first + ":" + gameUpdate.second + ",";
+    }
+    // copilot generated - seems like an iterator over the map and seems to do the trick. check if this the right output
+    headers["game updates"] = gameUpdatesString;
+    headers["description"] = event.get_discription();
+    SendFrame *frame = new SendFrame(StompCommand::SEND, headers, "");
+    return frame;
 }
