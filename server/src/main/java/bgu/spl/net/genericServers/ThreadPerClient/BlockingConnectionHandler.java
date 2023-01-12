d13fac6dbc77da2fa7ca5acafa4c9cf36ccb0e27
Yuval Roth <rothyuv@post.bgu.ac.il>
2023-01-13 01:38:23 +0200

    major breakthroughs. login,logout,join work good. report has a problem that it doesn't show the reporter username

--- server/src/main/java/bgu/spl/net/genericServers/ThreadPerClient/BlockingConnectionHandler.java
+++ server/src/main/java/bgu/spl/net/genericServers/ThreadPerClient/BlockingConnectionHandler.java
@@ -61,7 +61,7 @@ public class BlockingConnectionHandler<T> implements Runnable, ConnectionHandler
                 }
             }
         } catch (IOException ex) {
-            ex.printStackTrace();
+            // ex.printStackTrace();
         }
     }
 
