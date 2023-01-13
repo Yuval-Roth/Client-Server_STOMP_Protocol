46331bf3a70c77c404f29ad270fd91c43577a941
Yuval Roth <rothyuv@post.bgu.ac.il>
2023-01-13 02:18:32 +0200

    implemented shouldTerminate in the server

--- server/src/main/java/bgu/spl/net/impl/stomp/Service/StompMessagingProtocolImpl.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/Service/StompMessagingProtocolImpl.java
@@ -2,10 +2,13 @@ package bgu.spl.net.impl.stomp.Service;
 
 import bgu.spl.net.api.MessagingProtocol;
 import bgu.spl.net.genericServers.interfaces.ConnectionHandler;
+import bgu.spl.net.impl.stomp.Service.STOMP_Frames.DisconnectFrame;
 import bgu.spl.net.impl.stomp.Service.STOMP_Frames.ErrorFrame;
 import bgu.spl.net.impl.stomp.Service.STOMP_Frames.ExecutableFrame;
 
 public class StompMessagingProtocolImpl implements MessagingProtocol<String> {
+
+    private boolean shouldTerminate = false;
 
     @Override
     public String process(ConnectionHandler<String> handler,String message) {
@@ -22,14 +25,17 @@ public class StompMessagingProtocolImpl implements MessagingProtocol<String> {
                                                     ,message,true).toString();
         }
         else{
-            return frame.execute(handler);
+            String output = frame.execute(handler);
+            if(frame.getClass() == DisconnectFrame.class){
+                shouldTerminate = true;
+            }
+            return output;
         }
     }
 
     @Override
     public boolean shouldTerminate() {
-        // TODO shouldTerminate
-        return false;
+        return shouldTerminate;
     }
 
 
